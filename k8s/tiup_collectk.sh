32349c5eb91695a4d70c26440c073b82b2d11a8f
Michael Zhang <michael.zhang@pingcap.com>
2026-08-09 15:00:11 -0400

    .

--- k8s/tiup_collectk.sh
+++ k8s/tiup_collectk.sh
@@ -6,11 +6,11 @@ set -uo pipefail
 # Configuration
 ###############################################################################
 
-cluster_name="shared3-prod-eks"
-namespace="shared3-prod"
+cluster_name="pikachu-prod-eks"
+namespace="pikachu-prod"
 
-begin="2026-08-06 13:00:00"
-end="2026-08-06 14:00:00"
+begin="2026-08-09 16:00:00"
+end="2026-08-09 18:30:00"
 
 # Interval in minutes.
 interval=30
