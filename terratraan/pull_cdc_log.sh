aa07426d6cf50730ac225612fb660fc935cafa16
Michael Zhang <michael.zhang@pingcap.com>
2026-09-04 14:36:42 -0400

    .

--- terratraan/pull_cdc_log.sh
+++ terratraan/pull_cdc_log.sh
@@ -4,7 +4,7 @@ set -u
 # ===== User variables =====
 CLUSTER="pingraph-notifications-prod"
 BEGIN_TIME="2026/09/03 17:50:00"
-END_TIME="2026/09/03 21:00:00"
+END_TIME="2026/09/03 19:15:00"
 
 # TiCDC log directory on ticdc nodes
 LOG_DIR="/var/log/tidb"
